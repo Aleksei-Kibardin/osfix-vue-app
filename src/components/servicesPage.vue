654c6c8e661255cd1020f061bd6566852257e00e
Aleksei-Kibardin <kibardin20000@gmail.com>
2024-02-29 18:28:42 +0300

    styled the services page

--- src/components/servicesPage.vue
+++ src/components/servicesPage.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="services-wrap">
-    <div
-      class="service"
-      v-for="(t, i) in Services"
-      :key="t"
-      :class="positionClass(i)"
-      :style="{ 'grid-area': areaValue[i] }"
-    >
-      <img class="service-img" :src="`${t.url}`" :alt="`${t.title}`" />
-      <div class="service-txt">
-        <h1>{{ t.title }}</h1>
-        <p>{{ t.description }}</p>
+  <div class="content">
+    <h1 class="title">Наши услуги</h1>
+    <div class="services-wrap">
+      <div
+        class="service"
+        v-for="(t, i) in Services"
+        :key="t"
+        :class="positionClass(i)"
+        :style="{ 'grid-area': areaValue[i] }"
+      >
+        <img class="service-img" :src="`${t.url}`" :alt="`${t.title}`" />
+        <div class="service-txt">
+          <h1>{{ t.title }}</h1>
+          <p>{{ t.description }}</p>
+        </div>
       </div>
+      <!-- <div class="midle"></div> -->
     </div>
-    <div class="midle"></div>
   </div>
 </template>
 
@@ -23,7 +26,24 @@ import { useStore } from "vuex";
 
 const store = useStore();
 
-const areaValue = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',]
+const areaValue = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+];
 
 const Services = ref(store.state.tabs);
 
@@ -41,52 +61,72 @@ const positionClass = (i) => {
 <style lang="scss" scoped>
 @import "../fluid.sass";
 
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.title{
+    color: white;
+    margin-top: 40px;
+}
 .services-wrap {
-    margin: 100px;
-  height: 100%;
-  width: 80vw;
+  @include fluid("padding", 20);
+  margin-top: 200px;
   display: grid;
-  gap: 10px;
+  gap: 80px;
   grid-template-areas:
-    "a a a z . . ."
-    ". . . z b b b"
-    "c c c z . . ."
-    ". . . z d d d"
-    "e e e z . . ."
-    ". . . z f f f"
-    "g g g z . . ."
-    ". . . z h h h"
-    "i i i z . . ."
-    ". . . z j j j"
-    "k k k z . . ."
-    ". . . z l l l"
-    "m m m z . . ."
-    ". . . z n n n"
-    "o o o z . . ."
-    ". . . z p p p";
+    "a a a a . . ."
+    ". . . b b b b"
+    "c c c c . . ."
+    ". . . d d d d"
+    "e e e e . . ."
+    ". . . f f f f"
+    "g g g g . . ."
+    ". . . h h h h"
+    "i i i i . . ."
+    ". . . j j j j"
+    "k k k k . . ."
+    ". . . l l l l"
+    "m m m m . . ."
+    ". . . n n n n"
+    "o o o o . . ."
+    ". . . p p p p";
 }
 .service {
-width: 800px;
+  @include fluid("width", 980);
   display: flex;
-  justify-content: space-around;
-  gap: 10px;
+  justify-content: center;
+  gap: 30px;
   color: white;
 }
 .right {
   flex-direction: row-reverse;
+  justify-content: end;
   text-align: end;
+  .service-img {
+    border-radius: 0 0 0 500px;
+  }
+}
+.left .service-img{
+    border-radius: 0 0 500px 0;
 }
 .midle {
-grid-area: z;
+  grid-area: z;
 }
 .service-img {
-  width: 500px;
-  height: 500px;
+  @include fluid("width", 500);
+  @include fluid("height", 500);
 }
-.service-txt{
-    align-self: center;
-    h1{
-        font-size: 40px;
-    }
+.service-txt {
+  align-self: center;
+
+  h1 {
+    @include fluid("font-size", 40);
+  }
+  p {
+    @include fluid("font-size", 18);
+  }
 }
 </style>
