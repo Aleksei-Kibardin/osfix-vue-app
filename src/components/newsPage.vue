50a3c675257eab569a3ce47daa3073d9b31130a6
Aleksei-Kibardin <kibardin20000@gmail.com>
2024-03-06 14:26:16 +0300

    finished creating the news page

--- src/components/newsPage.vue
+++ src/components/newsPage.vue
@@ -9,24 +9,49 @@
         alt=""
       />
     </div>
+    <div class="content">
+      <div class="wrap-news" v-for="t in store.state.news" :key="t">
+        <span class="data">{{ formattedDate(t.date) }}</span>
+        <div class="news">
+          <h2 class="news-title">
+            <div class="y-square"></div>
+            {{ t.title }}
+          </h2>
+          <p class="news-txt">{{ t.description }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import currentPage from "./currentPage.vue";
+import { useStore } from "vuex";
+
+const store = useStore();
 
 let currentDate = new Date();
 
 let previousMonth = currentDate.getMonth() === 0 ? 11 : currentDate.getMonth();
 
-let formattedDate =
-  ("0" + currentDate.getDate()).slice(-2) +
-  "." +
-  ("0" + previousMonth).slice(-2) +
-  "." +
-  currentDate.getFullYear();
+let formattedDate = (x) => {
+  let days;
+  if (x > 9) {
+    days = x;
+  } else {
+    days = "0" + x;
+  }
 
-console.log(formattedDate);
+  let date =
+    days +
+    "." +
+    ("0" + previousMonth).slice(-2) +
+    "." +
+    currentDate.getFullYear();
+
+  return date;
+};
+console.log(formattedDate(4));
 </script>
 
 <style lang="scss" scoped>
@@ -39,6 +64,46 @@ console.log(formattedDate);
 }
 .head {
   width: 100%;
-  @include fluid("height", 1000);
+  @include fluid("height", 500);
+}
+.content {
+  display: flex;
+  padding: 50px;
+  gap: 30px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+.data {
+  position: absolute;
+  right: 100px;
+}
+.wrap-news {
+  position: relative;
+  color: white;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: 80%;
+  box-shadow: 0px 0px 10px 2px rgba(73, 73, 73, 0.3);
+  background: #2b2b2b;
+}
+.news {
+  width: 1000px;
+  .news-txt {
+    margin-top: 10px;
+  }
+}
+.news-title{
+  display: flex;
+  gap: 10px;
+  align-items: center
+}
+.y-square {
+  @include fluid("width", 20);
+  @include fluid("height", 20);
+  background: #ffd400;
 }
 </style>
