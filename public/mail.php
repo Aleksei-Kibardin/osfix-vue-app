bf1e454d52fcdeb6779db58981bd24b82cbc8796
Aleksei-Kibardin <kibardin20000@gmail.com>
2024-03-22 15:18:52 +0300

    Update mail.php

--- public/mail.php
+++ public/mail.php
@@ -15,7 +15,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'kibardin2000@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'GHBDTNRFRLTKF&2142'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'g1zE5mbRkH9C8vvDFSD1'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
