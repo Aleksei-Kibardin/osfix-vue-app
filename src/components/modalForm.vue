09bbc20e3d6608ffd1b7cfab1221391da62225b4
Aleksei-Kibardin <kibardin20000@gmail.com>
2024-03-22 15:06:41 +0300

    Update modalForm.vue

--- src/components/modalForm.vue
+++ src/components/modalForm.vue
@@ -64,7 +64,7 @@ const closeModal = (event) => {
 
 const submitForm = async () => {
   try {
-    const response = await fetch("/public/mail.php", {
+    const response = await fetch("/mail.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
